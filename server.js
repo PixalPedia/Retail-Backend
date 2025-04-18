0a4bdc3e112bed350d1744087d4c5b2373bd6ad8
PixalPedia <pediapixal@gmail.com>
2025-04-18 03:19:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -81,6 +81,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:3000', 
+      'http://localhost:3001',
       'http://127.0.0.1:5500', 
       process.env.FRONTEND_URL || 'https://your-deployed-frontend-url.com'
     ],
